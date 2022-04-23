3a60c5ee5d160e6226f7c5c8e16d04fa7ba26e80
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-04-24 01:24:42 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,39 @@
+GFG - Basic
+
+Given a positive integer, N. Find the factorial of N.
+  
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.factorial(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution{
+    static long factorial(int N){
+        // code here
+        long f =1;
+        
+       for(long i = 2; i <=N; i++)
+       {
+           f = f*i;
+       }
+       if(N == 0)
+       return 1;
+       else
+       return f;
+    }
+}
