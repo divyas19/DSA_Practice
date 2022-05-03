9d3cc2740cbe064c50cfd98b36f270db44a3a3a5
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-05-03 17:57:28 +0530

    Create Kadane's Algorithm.java

---
+++ Kadane's Algorithm.java
@@ -0,0 +1,60 @@
+GFG - Array
+
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+  
+// { Driver Code Starts
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int ans = Integer.MIN_VALUE;
+        int s= 0;
+        for(int i = 0; i < n; i++)
+        {
+            
+            s+= arr[i];
+             ans = Math.max(ans,s);
+        if(s < 0)
+            s = 0;
+        }
+       
+       return (long)ans; 
+       
+    }
+    
+}
+
