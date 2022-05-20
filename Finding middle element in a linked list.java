4b786b4e67b479ca38f4c7d54bb90e69cfa0a9ac
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-05-20 18:25:39 +0530

    Create Finding middle element in a linked list.java

---
+++ Finding middle element in a linked list.java
@@ -0,0 +1,91 @@
+GFG - Linked List
+
+Given a singly linked list of N nodes.
+The task is to find the middle of the linked list. For example, if the linked list is
+1-> 2->3->4->5, then the middle node of the list is 3.
+If there are two middle nodes(in case, when N is even), print the second middle element.
+For example, if the linked list given is 1->2->3->4->5->6, then the middle node of the list is 4.
+  
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+         // Your code here.
+         int c = 0;
+         Node n = head;
+         while(n != null)
+         {
+         n = n.next;
+         c++;
+         }
+         int pos = 0;
+         if(c%2 != 0)
+         pos = c/2+1;
+         else
+         {
+             pos = c/2+1;
+         }
+        
+        Node temp = head;
+        for(int i =1 ; i < pos; i++)
+        {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+}
