40956436c7a7560510f0074c088ef817400678cf
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-05-21 12:47:38 +0530

    Create Detect Loop in linked list.java

---
+++ Detect Loop in linked list.java
@@ -0,0 +1,107 @@
+GFG - Linked List
+
+Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.
+
+// { Driver Code Starts
+//Initial template code for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code here
+        Node t = head;
+        
+        // if(t == null)
+        // return false;
+    
+        ArrayList<Node> ar = new ArrayList<Node>();
+        while(t.next != null)
+        {
+            if( ar.contains(t.next ))
+            {
+             return true;
+            }
+            else
+            {
+                ar.add(t.next);
+                t = t.next;
+            }
+        }
+        return false;
+    }
+}
