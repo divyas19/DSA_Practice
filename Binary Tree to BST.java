a079be3a314305d543339ac2d541d13693ac097b
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-11-23 00:31:37 +0530

    Create Binary Tree to BST.java

---
+++ Binary Tree to BST.java
@@ -0,0 +1,158 @@
+GFG - BST
+
+Given a Binary Tree, convert it to Binary Search Tree in such a way that keeps the original structure of Binary Tree intact.
+  
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        printInorder(root.right);
+    }
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            Node rootA = g.binaryTreeToBST(root);
+            printInorder(rootA);
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*Structure of the node class is
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+} */
+
+
+class Solution
+{
+    // The given root is the root of the Binary Tree
+    // Return the root of the generated BST
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    public static Node insert(ArrayList<Integer> al,int s, int e)
+{
+ if(s>e){return null;}
+        int mid=(e+s+1)/2;
+        Node root=new Node(al.get(mid));
+        root.left=insert(al,s,mid-1);
+        root.right=insert(al,mid+1,e);
+        return root;
+}
+
+public static void inorder(Node root,ArrayList<Integer> al)
+{
+if(root == null)
+return;
+
+inorder(root.left,al);
+al.add(root.data);
+inorder(root.right,al);
+}
+
+    Node binaryTreeToBST(Node root)
+    {
+       // Your code here
+       
+       inorder(root,al);
+       Collections.sort(al);
+       //System.out.println(al);
+      
+       return insert(al,0,al.size()-1);
+    }
+}
+ 
