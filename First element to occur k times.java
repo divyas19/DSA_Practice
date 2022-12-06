adea2918cbd3143e9c9e42984cc8fd2645c66b7a
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-12-06 22:43:34 +0530

    Create First element to occur k times.java

---
+++ First element to occur k times.java
@@ -0,0 +1,44 @@
+GFG - Array
+
+Given an array of N integers. Find the first element that occurs at least K number of times.
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(arr, n, m));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int[] a, int n, int k) { 
+        
+        int freq[] = new int[10000];
+        for(int i =0; i< n; i++)
+        {
+            freq[a[i]]++;
+            if(freq[a[i]] ==k)
+            return a[i];
+        }
+        return -1;
+    } 
+}
