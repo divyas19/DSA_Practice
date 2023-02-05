2eb33ffb31e736f2c3f3f58e9e42435bda3edaff
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-05 23:02:05 +0530

    Create Trailing zeroes in factorial.java

---
+++ Trailing zeroes in factorial.java
@@ -0,0 +1,41 @@
+GFG - Logic
+
+For an integer N find the number of trailing zeroes in N!.
+  
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+			
+            Solution ob = new Solution();
+            int ans  = ob.trailingZeroes(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int trailingZeroes(int N){
+        // Write your code here
+        int ct =0;
+        for(int i = 5;N/i>0;i*=5)
+        {
+            ct+=N/i;
+        }
+        return ct;
+    }
+}
